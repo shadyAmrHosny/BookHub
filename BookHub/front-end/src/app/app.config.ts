9994c2523380756523a519bb03d8c8cd2e2b985c
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-06-18 20:26:51 +0300

    initial

---
+++ BookHub/front-end/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { routes } from './app.routes';
+import { provideZoneChangeDetection } from '@angular/core';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(BrowserModule, HttpClientModule, FormsModule)
+  ]
+};
